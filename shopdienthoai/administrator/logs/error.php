08157f065bb6b47cdf525567e564ac177ab34f40
cuong <truongquoccuongbt@gmail.com>
2017-04-23 11:55:32 +0700

    demo

---
+++ shopdienthoai/administrator/logs/error.php
@@ -0,0 +1,19 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-04-20 02:41:39 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-04-20T02:41:39+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T03:00:29+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T03:21:39+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T03:28:45+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T03:29:31+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T03:29:40+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T03:52:30+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T03:54:27+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T04:08:32+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T05:21:55+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-20T06:03:14+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-21T04:23:24+00:00	INFO ::1	joomlafailure	Thành viên không tồn tại
+2017-04-21T04:36:40+00:00	INFO ::1	joomlafailure	Sai mật khẩu
